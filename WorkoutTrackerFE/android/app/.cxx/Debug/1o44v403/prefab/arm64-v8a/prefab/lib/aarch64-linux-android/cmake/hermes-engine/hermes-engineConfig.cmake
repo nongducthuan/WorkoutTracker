f141134165b7489294399a93e5d0bfeb00a44da3
nongducthuan <150163180+nongducthuan@users.noreply.github.com>
2026-08-07 14:43:57 +0700

    Initial commit

---
+++ WorkoutTrackerFE/android/app/.cxx/Debug/1o44v403/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "C:/Users/HP/.gradle/caches/8.13/transforms/b2759a7b7ff6d558a3f11be552a86e66/transformed/hermes-android-250829098.0.16-debug/prefab/modules/hermesvm/libs/android.arm64-v8a/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/HP/.gradle/caches/8.13/transforms/b2759a7b7ff6d558a3f11be552a86e66/transformed/hermes-android-250829098.0.16-debug/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ WorkoutTrackerFE/android/app/.cxx/Debug/1o44v403/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "C:/Users/HP/Documents/GitHub/WorkoutTracker/WorkoutTrackerFE/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/5t33n341/obj/arm64-v8a/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/HP/Documents/GitHub/WorkoutTracker/WorkoutTrackerFE/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
