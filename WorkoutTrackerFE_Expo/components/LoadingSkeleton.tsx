bb31dd4eee9398f9bd5a3367fefa9c6ca97fece1
nongducthuan <150163180+nongducthuan@users.noreply.github.com>
2026-08-09 00:20:40 +0700

    Initial commit

--- WorkoutTrackerFE_Expo/components/LoadingSkeleton.tsx
+++
@@ -1,6 +0,0 @@
-import { View, ActivityIndicator } from 'react-native';
-export const DashboardSkeleton = () => (
-  <View className="flex-1 items-center justify-center bg-background">
-    <ActivityIndicator size="large" color="#C6F432" />
-  </View>
-);
--- WorkoutTrackerFE_Expo/components/MuscleMap.tsx
+++
@@ -1,6 +0,0 @@
-import { View, Text } from 'react-native';
-export const MuscleMap = (props: any) => (
-  <View className="items-center justify-center w-full h-40 bg-card/50 rounded-lg">
-    <Text className="text-muted-gray text-xs">Muscle Map Placeholder</Text>
-  </View>
-);
