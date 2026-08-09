c24227b9b0c1ef799edc80b09bc83f91f7811c9f
nongducthuan <150163180+nongducthuan@users.noreply.github.com>
2026-08-09 14:07:39 +0700

    Initial commit

--- WorkoutTrackerFE/src/api/client.ts
+++ WorkoutTrackerFE/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:3000'; // Replace with your actual backend URL
+const API_URL = 'http://10.0.2.2:8080';
 
 export const apiClient = axios.create({
   baseURL: API_URL,
